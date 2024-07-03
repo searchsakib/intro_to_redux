804014b63bf87df3ab50c419aaf265830e040e5e
searchsakib <searchsakib@gmail.com>
2024-07-03 23:52:11 +0600

    box added for each 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {
   decrement,
   increment,
@@ -8,9 +9,16 @@ import { useAppDispatch, useAppSelector } from './redux/hooks';
 function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+  const [boxesCount, setBoxesCount] = useState(0);
+  useEffect(() => {
+    const latestBoxCount = Math.floor(count / 5);
+    setBoxesCount(latestBoxCount);
+  }, [count]);
+
+  const boxes = Array.from({ length: boxesCount });
 
   return (
-    <div className='flex justify-center items-center gap-10 m-10'>
+    <div className='flex flex-col justify-center items-center gap-10 m-10'>
       <div className='flex border  border-yellow-300 rounded-md p-10 gap-10'>
         <button
           className='btn btn-success'
@@ -31,6 +39,11 @@ function App() {
         >
           Decrement
         </button>
+      </div>
+      <div className='grid grid-cols-10 gap-5'>
+        {boxes.map((box) => (
+          <div className='w-10 h-10 bg-red-500'> </div>
+        ))}
       </div>
     </div>
   );
